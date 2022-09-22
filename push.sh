2fa6ba71debaecd7eaf61f9b34a94a9ec9799deb
jason08122 <jason08122@gmail.com>
2022-09-22 15:53:22 +0800

    v3

---
+++ push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "git push..."
+function getmsg(){
+	read -p "please input the commit message: " message
+
+	if [ ! -n "$message" ];then
+		echo "need to input the commit message... "
+		getmsg
+	else
+		git commit -m "$message"
+	fi
+}
+
+git add .
+getmsg
+git push origin master
+echo "git push success... !"
--- test.cpp
+++ test.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main ()
 {
     cout<<"hello world!!"<<endl;
-    cout<<"hello world again!!"<<endl;
+    cout<<"hello world!!"<<endl;
     return 0;
 }
